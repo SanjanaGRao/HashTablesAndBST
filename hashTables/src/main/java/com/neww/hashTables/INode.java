ee9efcac9700f88485b2710867e9dc15fef30ad1
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-05 12:28:41 +0530

    Added: Interface INode to create a node for HashMap

---
+++ hashTables/src/main/java/com/neww/hashTables/INode.java
@@ -0,0 +1,14 @@
+package com.neww.hashTables;
+/*
+ * This public interface INode is used to define a node.
+ * @param K is of integer type.
+ * @author Sanjana Rao
+ * @since 5-09-2021
+ */
+public interface INode<K> 
+{	
+		K getKey();
+		void setKey(K key);
+		INode<K> getNext();
+		void setNext(INode<K> next);
+}
