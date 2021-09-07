cc1f1e5e2e0a95a03bf8e1ee8b7bc724886bbe6b
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-07 11:28:58 +0530

    Added: Ability for the user to create a tree as specified with more elements

--- hashTables/src/main/java/com/neww/hashTablesAndBST/BinarySearchTreeMain.java
+++ hashTables/src/main/java/com/neww/hashTablesAndBST/BinarySearchTreeMain.java
@@ -14,6 +14,16 @@ public class BinarySearchTreeMain
 		myBST.addElement(56);
 		myBST.addElement(30);
 		myBST.addElement(70);
+		myBST.addElement(22);
+		myBST.addElement(40);
+		myBST.addElement(11);
+		myBST.addElement(3);
+		myBST.addElement(16);
+		myBST.addElement(60);
+		myBST.addElement(95);
+		myBST.addElement(65);
+		myBST.addElement(63);
+		myBST.addElement(67);
 		System.out.println("The size of Binary Search Tree is: "+myBST.getSize());
 		System.out.println("Elements of the BST - PostOrder Traversal: ");
 		myBST.getNodes();
